698e28daba431ac91086bea1e8e4fd1ba3236e17
Uzair Ahmad <uzair247malik@gmail.com>
2026-07-22 17:06:22 +0500

    changes

--- Frontend/my-react-app/src/App.jsx
+++ Frontend/my-react-app/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <div className='text-red-600 text-4xl text-center'>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+
+      </BrowserRouter>
+
+
+    </>
   )
 }
 
